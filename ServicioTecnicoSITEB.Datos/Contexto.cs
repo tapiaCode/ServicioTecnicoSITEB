e5de1d7dbd1ea5922f5523c7f09f0ecb1986b3fe
Tapia <burbujeuky@gmail.com>
2023-08-21 17:55:35 -0400

    conexion a la base de datos, + referencias y paquetes nugets

---
+++ ServicioTecnicoSITEB.Datos/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicioTecnicoSITEB.Datos
+{
+    public class Contexto
+    {
+        public DBSITEPEntities TraerContexto()
+        {
+            DBSITEPEntities AuxCont = new DBSITEPEntities();
+            return AuxCont;
+        }
+    }
+}
