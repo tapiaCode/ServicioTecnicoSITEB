27cd0ec9ab6c2d555f8ba0c664ed4eb7fa870f71
Tapia <burbujeuky@gmail.com>
2023-08-22 19:29:05 -0400

    COMENTARIO PRUEBA

--- ServicioTecnicoSITEB.Negocios/RNEmpleado.cs
+++ ServicioTecnicoSITEB.Negocios/RNEmpleado.cs
@@ -30,5 +30,7 @@ namespace ServicioTecnicoSITEB.Negocios
 
             return (from e in Esquema.Empleado where e.Nombre_Empleado.ToUpper().StartsWith(nombreEmpleado.ToUpper()) select e).ToList();
         }
+
+        //CAMBIO NUEVO
     }
 }
