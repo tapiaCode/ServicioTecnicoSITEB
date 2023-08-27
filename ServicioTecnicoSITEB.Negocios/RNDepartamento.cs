5f8db58be0b1595cfe35f3e0c8f48f0bf44245be
Jhon Cruz <Jhon Cruz@JHON-CRUZ>
2023-08-27 18:03:01 -0400

    Creacion de 2 clases, RNDepartamenrto y Barrio

---
+++ ServicioTecnicoSITEB.Negocios/RNDepartamento.cs
@@ -0,0 +1,31 @@
+﻿using ServicioTecnicoSITEB.Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicioTecnicoSITEB.Negocios
+{
+    public class RNDepartamento : Contexto
+    {
+        DBSITEPEntities Esquema;
+
+        public RNDepartamento()
+        {
+            Esquema = this.TraerContexto();
+        }
+
+        public List<Departamento> TraerDepartamento(Int64 id)
+        {
+            if (id == 0)
+            {
+                return (from e in Esquema.Departamento select e).ToList();
+            }
+            else
+            {
+                return (from e in Esquema.Departamento where e.Id_Departamento.Equals(id) select e).ToList();
+            }
+        }
+    }
+}
