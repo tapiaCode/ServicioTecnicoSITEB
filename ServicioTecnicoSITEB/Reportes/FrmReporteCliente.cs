3d4b0ccbbbbab2ec113af4c70a690f584642786e
Jhon Cruz <Jhon Cruz@JHON-CRUZ>
2023-08-28 15:29:49 -0400

    Creacion de carpeta Reporte e Informes

---
+++ ServicioTecnicoSITEB/Reportes/FrmReporteCliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServicioTecnicoSITEB.Reportes
+{
+    public partial class FrmReporteCliente : Form
+    {
+        public FrmReporteCliente()
+        {
+            InitializeComponent();
+        }
+    }
+}
