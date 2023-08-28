3ee0759b3c0611ad78f2944ad9882b528413f865
Jhon Cruz <Jhon Cruz@JHON-CRUZ>
2023-08-28 15:51:35 -0400

    Instalacion de Microsoft.ReportingServices.ReportViewerControl.Winforms y actualisacion de Microsoft.SqlServer.Types

--- ServicioTecnicoSITEB/Reportes/FrmReporteCliente.cs
+++ ServicioTecnicoSITEB/Reportes/FrmReporteCliente.cs
@@ -16,5 +16,13 @@ namespace ServicioTecnicoSITEB.Reportes
         {
             InitializeComponent();
         }
+
+        private void FrmReporteCliente_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'reportes.GenerateClienteReport' Puede moverla o quitarla según sea necesario.
+            this.generateClienteReportTableAdapter.Fill(this.reportes.GenerateClienteReport);
+
+            this.reportViewer1.RefreshReport();
+        }
     }
 }
